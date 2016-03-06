b3cdb028c53d8e76de464c7704b5c80107d5b53a
sunhongyin <sunhongyin@sunhongyindemacbook-pro.local>
2016-03-06 12:26:05 +0800

    Initial Commit

---
+++ sqrt/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sqrt
+//
+//  Created by sunhongyin on 16/3/6.
+//  Copyright © 2016年 sunhongyin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
